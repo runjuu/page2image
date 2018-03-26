c37f0fa1fff2d6e5c03694bedec244b50356b6be
Runjuu <huang@runjuu.com>
2018-03-26 17:07:01 +0800

    demo - update demo code

--- demo/demo.js
+++ demo/demo.js
@@ -2,15 +2,8 @@ const Screenshot = require('../').default;
 
 const screenshot = new Screenshot({
   waitUntil: 'networkidle2',
-  waitForFunction: function waitForFunction() {
-    window.imageList = window.imageList || Array.from(document.getElementsByTagName('img'));
-
-    return window.imageList.length <= window.imageList.reduce((loaded, imageElm) => (
-      imageElm.complete ? loaded + 1 : loaded
-    ), 0);
-  },
-  viewportConfig: { width: 1920, height: 1080 },
-  screenshotConfig: { fullPage: true, path: 'screenshot.png' },
+  viewportConfig: { width: 1920, height: 100 },
+  screenshotConfig: { fullPage: true, path: 'screenshot_with_full_page.png' },
   launchConfig: {
     args: [
       '--no-sandbox',
@@ -19,6 +12,12 @@ const screenshot = new Screenshot({
   },
 });
 
-screenshot
-  .takeScreenshot('https://github.com/Runjuu')
+screenshot.takeScreenshot('https://github.com/Runjuu')
+  .then(() => {
+    screenshot.updateConfig({
+      selector: '.js-contribution-graph',
+      screenshotConfig: { path: 'screenshot_with_element.png' },
+    });
+    return screenshot.takeScreenshot('https://github.com/Runjuu');
+  })
   .then(process.exit);
